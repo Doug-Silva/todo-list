a73bb4d2594cde8572b3c4e9bd9da048fe66faed
Doug-Silva <silvatec.br@gmail.com>
2023-07-08 01:40:36 -0300

    finalizando o projeto

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@
         PUSH_NOTIFICATION = 'PUSH_NOTIFICATION',
     }
 
+    enum ViewMode {
+        TODO = 'TODO',
+        REMINDER = 'REMINDER',
+    }
+
     //função para atribuir um ID
     const UUID = (): string => {
         return Math.random().toString(32).substring(2, 9);
@@ -91,7 +96,27 @@
 
     //criando a view
     const taskView = {
-        render(tasks: Array<Task>) {
+        getTodo(form: HTMLFormElement): Todo {
+            const todoDescription = form.todoDescription.value;
+            form.reset();
+        return new Todo(todoDescription);
+        },
+        
+        getReminder(form: HTMLFormElement): Reminder {
+            const reminderNotifications = [
+                form.notification.value as NotificationPlatform,
+            ];
+            const reminderDate = new Date(form.scheduleDate.value);
+            const reminderDescription = form.reminderDescription.value;
+            form.reset();
+            return new Reminder(
+                reminderDescription,
+                reminderDate,
+                reminderNotifications
+            );
+        },
+
+        render(tasks: Array<Task>, mode: ViewMode) {
             const tasksList = document.getElementById('tasksList');
 
             //esvaziar a lista
@@ -104,19 +129,58 @@
                 const textNode = document.createTextNode(task.render());
                 li.appendChild(textNode);
                 tasksList?.appendChild(li);
-            })
+            });
+
+            //seletores: TODO set, Reminder set
+            const todoSet = document.getElementById("todoSet");
+            const reminderSet = document.getElementById("reminderSet");
+
+            if (mode === ViewMode.TODO) {
+                todoSet?.setAttribute("style", "display: block;");
+                todoSet?.removeAttribute("disabled");
+                reminderSet?.setAttribute("style", "display: none;");
+                reminderSet?.setAttribute("disabled", "true");
+            } else {
+                reminderSet?.setAttribute("style", "display: block;");
+                reminderSet?.removeAttribute("disabled");
+                todoSet?.setAttribute("style", "display: none;");
+                todoSet?.setAttribute("disabled", "true");
+            }
         },
     };
 
     //criando controller
     const TaskController = (view: typeof taskView) => {
-        const tasks: Array<Task> = [todo, reminder];
+        const tasks: Array<Task> = [];
+        let mode: ViewMode = ViewMode.TODO;
 
         const handleEvent = (event: Event) => {
             event.preventDefault();
-            view.render(tasks);
+            const form = event.target as HTMLFormElement;
+            switch (mode as ViewMode) {
+                case ViewMode.TODO:
+                    tasks.push(view.getTodo(form));
+                    break;
+                case ViewMode.REMINDER:
+                    tasks.push(view.getReminder(form));
+                    break;
+            }
+            view.render(tasks, mode);
         };
 
+        const handleToggleMode = () => {
+            switch (mode as ViewMode) {
+                case ViewMode.TODO:
+                    mode = ViewMode.REMINDER
+                    break;
+                case ViewMode.REMINDER:
+                    mode = ViewMode.TODO
+                    break;
+            }
+            view.render(tasks, mode);
+        };
+
+        document.getElementById('toggleMode')?.addEventListener('click', handleToggleMode);
         document.getElementById('taskForm')?.addEventListener('submit', handleEvent);
     };
 
